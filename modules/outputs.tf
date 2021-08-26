1596461ebb6128882a0757d516a17ce0fd4e3e30
Viet Pham <37876479+vietpham123@users.noreply.github.com>
2021-08-26 17:10:49 -0500

    Create outputs.tf

---
+++ modules/outputs.tf
@@ -0,0 +1,12 @@
+# Outputs to use with VM building
+output "virtual_network" {
+    value = azurerm_virtual_network.hashinet.name
+}
+
+output "subnet_name" {
+    value = azurerm_subnet.hashisubnet.id
+}
+
+output "nsg" {
+    value = azurerm_network_security_group.hashinsg.id
+}
