aed9f8350c88a797f2e25666cb5f34268800f60e
Viet Pham <37876479+vietpham123@users.noreply.github.com>
2021-08-26 17:08:23 -0500

    Update network.tf

--- network.tf
+++ network.tf
@@ -48,16 +48,3 @@ resource "azurerm_network_security_group" "hashinsg" {
     destination_address_prefix = "*"
   }
 }
-
-# Outputs to use with VM building
-output "virtual_network" {
-    value = azurerm_virtual_network.hashinet.name
-}
-
-output "subnet_name" {
-    value = azurerm_subnet.hashisubnet.id
-}
-
-output "nsg" {
-    value = azurerm_network_security_group.hashinsg.id
-}
