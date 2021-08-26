508b69d785b677bc7eda96b2d5755da353b2c9d8
Viet Pham <37876479+vietpham123@users.noreply.github.com>
2021-08-26 17:18:37 -0500

    Create main.tf

---
+++ modules/network/main.tf
@@ -0,0 +1,50 @@
+# Create virtual network
+resource "azurerm_virtual_network" "hashinet" {
+  name                = "vpVnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = var.hashiregion
+  resource_group_name = var.hashirg
+
+  tags = {
+    environment = "Terraform Demo"
+  }
+}
+
+# Create subnet
+resource "azurerm_subnet" "hashisubnet" {
+  name                 = "vpSubnet"
+  resource_group_name  = var.hashirg
+  virtual_network_name = azurerm_virtual_network.hashinet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# Create Network Security Group and Rule
+resource "azurerm_network_security_group" "hashinsg" {
+  name                = "vpNetworkSecurityGroup"
+  location            = var.hashiregion
+  resource_group_name = var.hashirg
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "raddit"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "9292"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
