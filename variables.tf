28edcd5cda51313c47d55a3b396848e0a4f5126f
Viet Pham <37876479+vietpham123@users.noreply.github.com>
2021-08-26 20:18:52 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,13 +7,3 @@ variable "hashiregion" {
   type = string
   default = ""
 }
-
-variable "vpc_subnet" {
-  type = string
-  default = ""
-}
-
-variable "vpc_nsg" {
-  type = string
-  default = ""
-}
